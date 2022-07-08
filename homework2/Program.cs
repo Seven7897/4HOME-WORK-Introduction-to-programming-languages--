c0f8302ec3ba51ed05865fbffa822a8a42e22260
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-08 22:12:37 +0300

    *Второе_задание*

---
+++ homework2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0 ;
+while(number != 0 )
+{
+    result = result + ( number % 10 ) ;
+    number /= 10 ;
+}
+Console.WriteLine();
+Console.WriteLine(result);
+
+
+/*Console.WriteLine("Введите строку:");
+string? input = Console.ReadLine();
+ 
+bool result = int.TryParse(input, out var number);
+if (result == true)
+    Console.WriteLine($"Преобразование прошло успешно. Число: {number}");
+else
+    Console.WriteLine("Преобразование завершилось неудачно");
+*/
