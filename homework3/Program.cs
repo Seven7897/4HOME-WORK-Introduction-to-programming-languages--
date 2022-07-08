d4f3228e5104df1c8c7baa3fccc427ca9463f952
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-08 22:32:29 +0300

    *Третье_задание*

---
+++ homework3/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Массив из восьми элементов , для подтверждения нажмите цифру : 8 ");
+int number = Convert.ToInt32(Console.ReadLine());
+void FillArray (int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while(index < collection.Length)
+    {
+        collection[index] = new Random().Next(1,20);
+        index++;
+    }
+}
+
+
+if (number < 8 || number > 8)
+{
+    Console.WriteLine("Вы не подтвердили , повторите попытку ");
+    return;
+}
+int[] array = new int[number];
+
+FillArray(array);
+
+
+
+Console.WriteLine($"[{String.Join(";",array)}]");
